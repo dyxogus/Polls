0e522837ea8b7fd37fc291c0c798343c7c43a238
Taehyun Lee <taehyunlee12@gmail.com>
2017-03-15 18:26:05 +0000

    Part 2A - Basic Model and migration 0001 added

--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
     return HttpResponse('Hello, you are at index')
 
 
 def something(request):
-    return render()
+    return render('', {})
