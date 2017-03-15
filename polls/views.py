f40f18b26fcc42d806ff88e97f0f6240d6437c66
Taehyun Lee <taehyunlee12@gmail.com>
2017-03-15 22:54:33 +0000

    Part 3B' - Refactoring with get_object_or_404

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from polls.models import Question
 
@@ -15,12 +15,9 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
 
-        context = {'question': question}
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
+    context = {'question': question}
 
     return render(request, 'polls/detail.html', context)
 
